fd0f2d5bffaa625483cee1d5c6472c027177d7b4
Kairat Sultan <garri.sultonov@mail.ru>
2023-11-13 18:48:54 +0600

    check

--- tgbot/handlers/add_channel.py
+++ tgbot/handlers/add_channel.py
@@ -14,20 +14,21 @@ from tgbot.misc.states import AddChannel
 async def get_channel_msg(m: types.Message, db: Database, state: FSMContext):
     data = await state.get_data()
     if m.is_forward() and m.forward_from_chat.type == "channel" and 'guid' in data:
-        try:
-            await m.bot.get_chat_administrators(m.forward_from_chat.id)
-            try:
-                await db.create_channel(data['guid'], str(m.forward_from_chat.id), m.forward_from_chat.title)
-                await m.answer("Sizning kanalingiz platformaga qo'shildi")
-                await state.finish()
-            except ClientResponseError:
-                await m.answer("Server bilan bog'lanishdagi xato, birozdan so'ng urunib ko'ring")
-            except ClientError:
-                await m.answer("Ushbu kanal allaqachon qo'shilgan")
+        # try:
+        await m.send_copy(m.from_user.id)
+        await m.bot.get_chat_administrators(m.forward_from_chat.id)
+        # try:
+        await db.create_channel(data['guid'], str(m.forward_from_chat.id), m.forward_from_chat.title)
+        await m.answer("Sizning kanalingiz platformaga qo'shildi")
+        await state.finish()
+            # except ClientResponseError:
+            #     await m.answer("Server bilan bog'lanishdagi xato, birozdan so'ng urunib ko'ring")
+            # except ClientError:
+            #     await m.answer("Ushbu kanal allaqachon qo'shilgan")
 
-        except Unauthorized:
-            await m.answer("Ushbu bot kanalda admin sifatida qayd etilmagan, "
-                           "iltimos kanalga admin sifatida qo'shib keyin kanaldan xabaringizni yo'naltiring")
+        # except Unauthorized:
+        #     await m.answer("Ushbu bot kanalda admin sifatida qayd etilmagan, "
+        #                    "iltimos kanalga admin sifatida qo'shib keyin kanaldan xabaringizni yo'naltiring")
     elif 'guid' not in data:
         await m.answer("Platformadan qaytadan kanal yaratish tugmasini bosing, bog'lanish uzildi.")
     else:
